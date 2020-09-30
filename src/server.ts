cffcd213b3557df8f20bf97b88a62cc5494e0f8c
Moritz Tietze <moritz.tietze@icloud.com>
2020-09-30 22:21:37 +0200

    docker composing

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
 console.log("Hello World")
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
