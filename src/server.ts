f1db8aa60507584af1a0a089af4fba21bce3782c
Moritz Tietze <moritz.tietze@icloud.com>
2020-10-01 08:14:37 +0200

    made database creds safe

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { app } from "./express/app"
+import mongoose from "mongoose";
 
-require('dotenv').config()
+mongoose.connect("mongodb://fffapp:fffapp@mongo-db:27017/fffapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT || 8080}`)
