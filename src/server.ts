37fbbd41abf22ac1340dceb318cc9efba4dc493f
Moritz Tietze <moritz.tietze@icloud.com>
2020-10-01 18:03:50 +0200

    mongodb volume management + removed authentification

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
-import { app } from "./express/app"
+import { app } from "./express/app";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://fffapp:fffapp@mongo-db:27017/fffapp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+console.log("Connecting to Database");
+mongoose.connect("mongodb://mongo:27017/abizitate", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
+console.log("Starting to listen")
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Example app listening at http://localhost:${process.env.PORT || 8080}`)
-})
+	console.log(`Listening at port ${process.env.PORT || 8080}`);
+});
 
