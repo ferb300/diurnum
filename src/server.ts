e71f45fb763ad0673464d78fbd84dc6fe0dd8eab
Moritz Tietze <moritz.tietze@icloud.com>
2022-02-21 17:38:32 +0100

    updated mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import { app } from "./express/app";
 
-import mongoose, { ConnectionOptions } from "mongoose";
+import mongoose from "mongoose";
 import "dotenv/config"
 
 
 console.log("Connecting to Database");
-mongoose.connect("mongodb://mongo:27017/abizitate", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-} as ConnectionOptions);
+mongoose.connect("mongodb://mongo:27017/abizitate");
 
 console.log("Starting to listen")
 app.listen(process.env.PORT || 8080, () => {
